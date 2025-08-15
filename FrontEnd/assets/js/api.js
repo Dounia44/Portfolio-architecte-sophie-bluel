4fb318d9240e07489225bb9713f3bb771126473e
Dounia <dounia.huber@gmail.com>
2025-08-15 14:59:06 +0200

    refactor: déplacer getWorks de app.js vers api.js

---
+++ FrontEnd/assets/js/api.js
@@ -0,0 +1,5 @@
+// Appels au serveur (fetch, gestion des erreurs, ..)
+export async function getWorks () {
+    const response = await fetch("http://localhost:5678/api/works");
+    return response.json();
+}
--- FrontEnd/assets/js/app.js
+++
@@ -1,5 +0,0 @@
-// Appels au serveur (fetch, gestion des erreurs, ..)
-export async function getWorks () {
-    const response = await fetch("http://localhost:5678/api/works");
-    return response.json();
-}
