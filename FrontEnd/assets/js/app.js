ab4314d05334da7e3cd9beed74cd1324aaeeea1e
Dounia <dounia.huber@gmail.com>
2025-08-15 14:54:33 +0200

    refactor: remplacer fetch .then() par async/await et exporter getWorks

--- FrontEnd/assets/js/app.js
+++ FrontEnd/assets/js/app.js
@@ -1,7 +1,5 @@
-fetch("http://localhost:5678/api/works")        // On demande les données au serveur
-    .then(function(response) {                  // Quand le serveur répond      =>    .then(response => response.json())
-        return response.json();                 // On transforme la réponse en JSON                 
-    })
-    .then(function(data){                       // Quand le JSON est prêt       =>    .then(data => console.log(data))
-        console.log(data);                      // On affiche les données
-    })
+// Appels au serveur (fetch, gestion des erreurs, ..)
+export async function getWorks () {
+    const response = await fetch("http://localhost:5678/api/works");
+    return response.json();
+}
