71e6337dc6e7cfb5bd02563cbb9bac511fc21e63
Dounia <dounia.huber@gmail.com>
2025-08-18 10:43:45 +0200

    refactor: passer de innerHTML à  createElement + appendChild

--- FrontEnd/assets/js/app.js
+++ FrontEnd/assets/js/app.js
@@ -1,23 +1,28 @@
-app.js
 import { getCategories, getWorks } from "./api.js";
 
 function affichageWorks(works) {
     const gallery = document.querySelector(".gallery");
-    works.forEach((work) => {                           // Je fais une boucle sur le tableau works pour récupérer chaque travaux 1 par 1
+    gallery.replaceChildren();                                      //  Je vide la galerie en supprimant tous les enfants
 
-    gallery.innerHTML += `
-        <figure>
-            <img src="${work.imageUrl}" alt="${work.title}">
-            <figcaption>${work.title}</figcaption>
-        </figure>
-    `;
+    works.forEach((work) => {                                       //  Je fais une boucle sur le tableau works pour récupérer chaque travaux 1 par 1
+    const figure = document.createElement("figure");                //  je crée un élément "figure"
+    const img = document.createElement("img");                      //  je crée un élément "img"
+    img.src = work.imageUrl;                                        //  je définit l'URL de l'image
+    img.alt = work.title;                                           //  je définit le texte alternatif de l'image
+    const caption = document.createElement("figcaption");           //  je crée un élément "figcaption" caption.textContent = work.title;
+    caption.textContent = work.title;
+    
+    figure.appendChild(img);                                        // Ajoute (img) dans <figure>
+    figure.appendChild(caption);                                    // Ajoute (caption) dans <figure>                                
+    gallery.appendChild(figure);                                    // Ajoute le <figure> complet dans la galerie
 });
 }
+
 async function init() {
-   const works = await getWorks();
-   const categories = await getCategories();
-   affichageWorks(works);
-   console.log(works, categories);
+    const works = await getWorks();
+    const categories = await getCategories();
+    affichageWorks(works);
+    console.log(works, categories);
 }
 
-init();
+init(); 
