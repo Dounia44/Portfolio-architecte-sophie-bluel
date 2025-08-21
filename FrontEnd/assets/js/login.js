f21537126bd39260f32a85e2d2ca8f52c5c20c0a
Dounia <dounia.huber@gmail.com>
2025-08-21 09:39:11 +0200

    feat(auth): implémentation de la connexion utilisateur (fetch API, stockage token, redirection)

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,1 +1,46 @@
-//Gestion de formulaire de connexion
+//Ecouter l'événement global submit du formulaire 
+const form = document.querySelector("form");    //Je selectionne le formulaire dans la page login.html
+const monEmail = document.getElementById("email");         //j'identifie les champs input
+const monPassword = document.getElementById("password");    //j'identifie les champs password
+
+form.addEventListener("submit", async (event) => {    //Écoute du submit
+    event.preventDefault();                     //Bloque le chargement automatique 
+
+//on récupère ce que l'utilisateur a tapé
+    const emailValue = monEmail.value;      
+    const passwordValue = monPassword.value;
+
+    console.log(emailValue, passwordValue);         //tester les valeurs 
+
+    try {
+        // 2.Envoyer la requête POST avec fetch() :
+        const response = await fetch("http://localhost:5678/api/users/login", {
+            method: "POST",                          // méthode HTTP : POST
+            headers: {
+                "content-type": "application/json"   // indique qu'on envoie du JSON
+            },
+            body: JSON.stringify({                  // conversion objet JS en JSON
+                email: emailValue,
+                password: passwordValue
+            })
+        });
+        // 3. Vérifier que la réponse est correcte 
+        if (! response.ok) {
+           throw new Error("Erreur lors de la connexion");
+        }
+
+        // 4. Lire la réponse JSON de l'API 
+        const data = await response.json();      // on attend la réponse convertie en JSON
+        console.log("réponse API :", data);
+
+        // Stocker le token
+        localStorage.setItem("token", data.token);
+
+        // Rediriger vers la page d'accueil
+        window.location.href = "./index.html";
+
+        // 5. Gérer les erreurs réseau ou de traitement :
+    } catch (error) {
+        console.error("Erreur :", error);
+    }
+});
